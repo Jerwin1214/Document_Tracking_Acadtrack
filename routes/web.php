f7f58ae661d53d6da3b495e480bb314edf8aa1ea
Dasun <techsaralk.pro@gmail.com>
2024-10-05 21:48:59 +0530

    routes updated with prefixes

--- routes/web.php
+++ routes/web.php
@@ -28,74 +28,74 @@ Route::get('/register', [StudentRegisterController::class, 'create'])->name('reg
 Route::post('/register', [StudentRegisterController::class, 'store'])->name('register');
 
 // Admin routes
-Route::middleware(['auth', AdminMiddleware::class])->group(function () {
-    Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+Route::middleware(['auth', AdminMiddleware::class])->prefix('admin')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
 
     // students
-    Route::get('/admin/students/show', [StudentController::class, 'showAllStudents'])->name('admin.students.index');
-    Route::get('/admin/students/create', [StudentController::class, 'create'])->name('admin.students.create');
-    Route::post('/admin/students', [StudentController::class, 'store'])->name('admin.students.store');
-    Route::get('/admin/students/{student}/edit', [StudentController::class, 'edit'])->name('admin.students.edit');
-    Route::patch('/admin/students/{student}', [StudentController::class, 'update'])->name('admin.students.update');
-    Route::get('/admin/students/{student}', [StudentController::class, 'show'])->name('admin.students.show');
-    Route::delete('/admin/students/{student}', [StudentController::class, 'destroy'])->name('admin.students.destroy');
+    Route::get('/students/show', [StudentController::class, 'showAllStudents'])->name('admin.students.index');
+    Route::get('/students/create', [StudentController::class, 'create'])->name('admin.students.create');
+    Route::post('/students', [StudentController::class, 'store'])->name('admin.students.store');
+    Route::get('/students/{student}/edit', [StudentController::class, 'edit'])->name('admin.students.edit');
+    Route::patch('/students/{student}', [StudentController::class, 'update'])->name('admin.students.update');
+    Route::get('/students/{student}', [StudentController::class, 'show'])->name('admin.students.show');
+    Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('admin.students.destroy');
 
     // teachers
-    Route::get('/admin/teachers/show', [TeacherController::class, 'showAllTeachers'])->name('admin.teachers.index');
-    Route::get('/admin/teachers/create', [TeacherController::class, 'create'])->name('admin.teachers.create');
-    Route::post('/admin/teachers', [TeacherController::class, 'store'])->name('admin.teachers.store');
-    Route::get('/admin/teachers/{teacher}', [TeacherController::class, 'show'])->name('admin.teacher.show');
-    Route::get('/admin/teachers/{teacher}/edit', [TeacherController::class, 'edit'])->name('admin.teachers.edit');
-    Route::patch('/admin/teachers/{teacher}', [TeacherController::class, 'update'])->name('admin.teachers.update');
-    Route::delete('/admin/teachers/{teacher}', [TeacherController::class, 'destroy'])->name('admin.teachers.destroy');
+    Route::get('/teachers/show', [TeacherController::class, 'showAllTeachers'])->name('admin.teachers.index');
+    Route::get('/teachers/create', [TeacherController::class, 'create'])->name('admin.teachers.create');
+    Route::post('/teachers', [TeacherController::class, 'store'])->name('admin.teachers.store');
+    Route::get('/teachers/{teacher}', [TeacherController::class, 'show'])->name('admin.teacher.show');
+    Route::get('/teachers/{teacher}/edit', [TeacherController::class, 'edit'])->name('admin.teachers.edit');
+    Route::patch('/teachers/{teacher}', [TeacherController::class, 'update'])->name('admin.teachers.update');
+    Route::delete('/teachers/{teacher}', [TeacherController::class, 'destroy'])->name('admin.teachers.destroy');
 
     // subjects
-    Route::get('/admin/subjects/show', [SubjectController::class, 'showAllSubjects'])->name('admin.subjects.index');
-    Route::get('/admin/subjects/create', [SubjectController::class, 'create'])->name('admin.subjects.create');
-    Route::post('/admin/subjects', [SubjectController::class, 'store'])->name('admin.subjects.store');
-    Route::get('/admin/subjects/{subject}/edit', [SubjectController::class, 'edit'])->name('admin.subjects.edit');
-    Route::patch('/admin/subjects/{subject}', [SubjectController::class, 'update'])->name('admin.subjects.update');
-    Route::delete('/admin/subjects/{subject}', [SubjectController::class, 'destroy'])->name('admin.subjects.destroy');
-    Route::get('/admin/subjects/assign', [SubjectController::class, 'assignTeachersView'])->name('admin.subjects.assignView');
-    Route::post('/admin/subjects/assign', [SubjectController::class, 'assignTeachers'])->name('admin.subjects.assign');
-    Route::get('/admin/subjects/teachers/{teacher}', [SubjectController::class, 'showAssignedSubjectsForTeacher'])->name('admin.subjects.teachers');
+    Route::get('/subjects/show', [SubjectController::class, 'showAllSubjects'])->name('admin.subjects.index');
+    Route::get('/subjects/create', [SubjectController::class, 'create'])->name('admin.subjects.create');
+    Route::post('/subjects', [SubjectController::class, 'store'])->name('admin.subjects.store');
+    Route::get('/subjects/{subject}/edit', [SubjectController::class, 'edit'])->name('admin.subjects.edit');
+    Route::patch('/subjects/{subject}', [SubjectController::class, 'update'])->name('admin.subjects.update');
+    Route::delete('/subjects/{subject}', [SubjectController::class, 'destroy'])->name('admin.subjects.destroy');
+    Route::get('/subjects/assign', [SubjectController::class, 'assignTeachersView'])->name('admin.subjects.assignView');
+    Route::post('/subjects/assign', [SubjectController::class, 'assignTeachers'])->name('admin.subjects.assign');
+    Route::get('/subjects/teachers/{teacher}', [SubjectController::class, 'showAssignedSubjectsForTeacher'])->name('admin.subjects.teachers');
 
     // classes
-    Route::get('/admin/class/create', [ClassController::class, 'create'])->name('admin.classes.create');
-    Route::post('/admin/class', [ClassController::class, 'store'])->name('admin.classes.store');
-    Route::get('/admin/class/show', [ClassController::class, 'index'])->name('admin.classes.index');
-    Route::get('/admin/class/{class}', [ClassController::class, 'show'])->name('admin.classes.show');
-    Route::get('/admin/class/{class}/edit', [ClassController::class, 'edit'])->name('admin.classes.edit');
-    Route::patch('/admin/class/{class}', [ClassController::class, 'update'])->name('admin.classes.update');
-    Route::delete('/admin/class/{class}', [ClassController::class, 'destroy'])->name('admin.classes.destroy');
-    Route::get('/admin/class/{class}/assign', [ClassController::class, 'assignStudentsView'])->name('admin.classes.assignView');
-    Route::post('/admin/class/{class}/assign', [ClassController::class, 'assignStudents'])->name('admin.classes.assign');
+    Route::get('/class/create', [ClassController::class, 'create'])->name('admin.classes.create');
+    Route::post('/class', [ClassController::class, 'store'])->name('admin.classes.store');
+    Route::get('/class/show', [ClassController::class, 'index'])->name('admin.classes.index');
+    Route::get('/class/{class}', [ClassController::class, 'show'])->name('admin.classes.show');
+    Route::get('/class/{class}/edit', [ClassController::class, 'edit'])->name('admin.classes.edit');
+    Route::patch('/class/{class}', [ClassController::class, 'update'])->name('admin.classes.update');
+    Route::delete('/class/{class}', [ClassController::class, 'destroy'])->name('admin.classes.destroy');
+    Route::get('/class/{class}/assign', [ClassController::class, 'assignStudentsView'])->name('admin.classes.assignView');
+    Route::post('/class/{class}/assign', [ClassController::class, 'assignStudents'])->name('admin.classes.assign');
 
     // profile
-    Route::get('/admin/profile', [AdminController::class, 'showProfile'])->name('admin.profile');
+    Route::get('/profile', [AdminController::class, 'showProfile'])->name('admin.profile');
 
     // settings
-    Route::get('/admin/settings', [AdminController::class, 'showSettings'])->name('admin.settings');
+    Route::get('/settings', [AdminController::class, 'showSettings'])->name('admin.settings');
     Route::post('/admin/settings', [AdminController::class, 'updateSettings'])->name('admin.settings.update');
 
     // messages
-    Route::get('/admin/messages', [AdminController::class, 'showMessages'])->name('admin.messages');
-    Route::get('/admin/messages/{message}', [AdminController::class, 'showMessage'])->name('admin.messages.show');
+    Route::get('/messages', [AdminController::class, 'showMessages'])->name('admin.messages');
+    Route::get('/messages/{message}', [AdminController::class, 'showMessage'])->name('admin.messages.show');
 
 });
 
 // Teacher routes
-Route::middleware(['auth', TeacherMiddleware::class])->group(function () {
+Route::middleware(['auth', TeacherMiddleware::class])->prefix('teacher')->group(function () {
     Route::get('/teacher/dashboard', [TeacherController::class, 'index'])->name('teacher.dashboard');
 
     // students section
-    Route::get('/teacher/students/add', [TeacherStudentController::class, 'create'])->name('teacher.students.create')->can('create', Student::class);
-    Route::post('/teacher/students', [TeacherStudentController::class, 'store'])->name('teacher.student.store')->can('create', Student::class);
-    Route::get('/teacher/students/show', [TeacherStudentController::class, 'showAllStudents'])->name('teacher.students.index');
-    Route::get('/teacher/students/{student}', [TeacherStudentController::class, 'show'])->name('teacher.students.show')->can('view', 'student');
-    Route::get('/teacher/students/{student}/edit', [TeacherStudentController::class, 'edit'])->name('teacher.students.edit')->can('update', 'student');
-    Route::patch('/teacher/students/{student}', [TeacherStudentController::class, 'update'])->name('teacher.students.update')->can('update', 'student');
-    Route::delete('/teacher/students/{student}', [TeacherStudentController::class, 'destroy'])->name('teacher.students.destroy');
+    Route::get('/students/add', [TeacherStudentController::class, 'create'])->name('teacher.students.create')->can('create', Student::class);
+    Route::post('/students', [TeacherStudentController::class, 'store'])->name('teacher.student.store')->can('create', Student::class);
+    Route::get('/students/show', [TeacherStudentController::class, 'showAllStudents'])->name('teacher.students.index');
+    Route::get('/students/{student}', [TeacherStudentController::class, 'show'])->name('teacher.students.show')->can('view', 'student');
+    Route::get('/students/{student}/edit', [TeacherStudentController::class, 'edit'])->name('teacher.students.edit')->can('update', 'student');
+    Route::patch('/students/{student}', [TeacherStudentController::class, 'update'])->name('teacher.students.update')->can('update', 'student');
+    Route::delete('/students/{student}', [TeacherStudentController::class, 'destroy'])->name('teacher.students.destroy');
 
     // profile
     Route::get('/teacher/profile', [TeacherMainController::class, 'showProfilePage'])->name('teacher.profile');
@@ -105,15 +105,15 @@ Route::middleware(['auth', TeacherMiddleware::class])->group(function () {
 });
 
 // Student routes
-Route::middleware(['auth', StudentMiddleware::class])->group(function () {
-    Route::get('/student/dashboard', [StudentController::class, 'index'])->name('student.dashboard');
+Route::middleware(['auth', StudentMiddleware::class])->prefix('student')->group(function () {
+    Route::get('/dashboard', [StudentController::class, 'index'])->name('student.dashboard');
 
     // profile
-    Route::get('/student/profile', [StudentStdController::class, 'showProfilePage'])->name('student.profile');
+    Route::get('/profile', [StudentStdController::class, 'showProfilePage'])->name('student.profile');
 
     // settings
-    Route::get('/student/settings', [StudentStdController::class, 'showSettingsPage'])->name('student.settings');
-    Route::post('/student/settings', [StudentStdController::class, 'updateSettings'])->name('student.settings.update');
+    Route::get('/settings', [StudentStdController::class, 'showSettingsPage'])->name('student.settings');
+    Route::post('/settings', [StudentStdController::class, 'updateSettings'])->name('student.settings.update');
 });
 
 
