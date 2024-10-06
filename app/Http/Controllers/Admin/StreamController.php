16582366d7892281c792e7c11eec33f6a5cea279
Dasun <techsaralk.pro@gmail.com>
2024-10-06 16:46:24 +0530

    StreamController added

---
+++ app/Http/Controllers/Admin/StreamController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\SubjectStream;
+use Illuminate\Http\Request;
+
+class StreamController extends Controller
+{
+    public function index() {
+        // TODO: Implement index() method.
+    }
+
+    public function create() {
+        // TODO: Implement create() method.
+    }
+
+    public function store(Request $request) {
+        // TODO: Implement store() method.
+    }
+
+    public function show(SubjectStream $subjectStream) {
+        // TODO: Implement show() method.
+    }
+
+    public function edit(SubjectStream $subjectStream) {
+        // TODO: Implement edit() method.
+    }
+
+    public function update(Request $request, SubjectStream $subjectStream) {
+        // TODO: Implement update() method.
+    }
+
+    public function destroy(SubjectStream $subjectStream) {
+        // TODO: Implement destroy() method.
+    }
+
+    public function assignSubjectsView(SubjectStream $subjectStream) {
+        // TODO: Implement assignSubjectsView() method.
+    }
+
+    public function assignSubjects(Request $request, SubjectStream $subjectStream) {
+        // TODO: Implement assignSubjects() method.
+    }
+}
