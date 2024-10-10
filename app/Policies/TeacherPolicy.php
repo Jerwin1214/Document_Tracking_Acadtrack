909ef30a6f4555b19001a4d3609dc463c8df8602
Dasun <techsaralk.pro@gmail.com>
2024-10-10 17:48:51 +0530

    Teacher policy added

--- app/Policies/TeacherPolicy.php
+++ app/Policies/TeacherPolicy.php
@@ -2,16 +2,16 @@
 
 namespace App\Policies;
 
-use App\Models\Student;
+use App\Models\Teacher;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class StudentPolicy
+class TeacherPolicy
 {
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): void // change this to bool
+    public function viewAny(User $user): void
     {
         //
     }
@@ -19,9 +19,9 @@ class StudentPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Student $student): void
+    public function view(User $user, Teacher $teacher): void
     {
-        // 
+        //
     }
 
     /**
@@ -29,21 +29,21 @@ class StudentPolicy
      */
     public function create(User $user): bool
     {
-        return $user->role->name === 'Admin' || $user->role->name === 'Teacher';
+        return $user->role->name === 'Admin';
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Student $student): bool
+    public function update(User $user, Teacher $teacher): bool
     {
-        return $user->role->name === 'Admin' || $user->role->name === 'Teacher';
+        return $user->role->name === 'Admin';
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Student $student): bool
+    public function delete(User $user, Teacher $teacher): bool
     {
         return $user->role->name === 'Admin';
     }
@@ -51,7 +51,7 @@ class StudentPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Student $student): void // change this to bool
+    public function restore(User $user, Teacher $teacher): void
     {
         //
     }
@@ -59,7 +59,7 @@ class StudentPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Student $student): void // change this to bool
+    public function forceDelete(User $user, Teacher $teacher): void
     {
         //
     }
