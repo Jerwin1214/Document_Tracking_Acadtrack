f7731817005c2e866eea3adc65aebf92e06507a3
Dasun <techsaralk.pro@gmail.com>
2024-10-11 23:06:51 +0530

    new migration made

--- database/migrations/2024_09_28_061644_create_subject_streams_table.php
+++ database/migrations/2024_09_28_061644_create_subject_streams_table.php
@@ -25,13 +25,6 @@ return new class extends Migration
             $table->foreignId('subject_stream_id')->constrained('subject_streams')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
-
-        Schema::create('custom_student_subjects', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('subject_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('student_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
-            $table->timestamps();
-        });
     }
 
     /**
