191e38188bdb15353d2aa117f428f7da061b590f
Dasun <techsaralk.pro@gmail.com>
2024-10-12 16:28:27 +0530

    post new announcement frontend finished

--- resources/views/pages/teachers/announcements/add.blade.php
+++ resources/views/pages/teachers/announcements/add.blade.php
@@ -4,7 +4,18 @@
 <h2>New Announcement</h2>
 <form action="/teacher/announcements/create" method="post" class="shadow-lg p-3 mb-5 mt-3 bg-body-tertiary rounded">
     @csrf
-
+    <div class="mb-3">
+        <label for="title" class="form-label">Title</label>
+        <input type="text" class="form-control" id="title" name="title" required>
+    </div>
+    <div class="mb-3">
+        <label for="description" class="form-label">Description</label>
+        <textarea class="form-control" id="description" name="description" rows="3" required></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="date" class="form-label">Date</label>
+        <input type="date" class="form-control" id="date" name="date" value="{{date('Y-m-d')}}" required readonly>
+    </div>
     <div class="mb-3">
         <button type="submit" class="btn btn-primary">Post Announcement</button>
         <button type="reset" class="btn btn-secondary">Clear</button>
