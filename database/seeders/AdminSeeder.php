5ed6f8d298b9ac817168a888fe104ac97aebbed3
Jerwin Uman <erwinarellano53@gmail.com>
2025-10-27 00:48:21 +0800

    updated admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -16,29 +16,35 @@ class AdminSeeder extends Seeder
     {
         $admins = [
             [
-                'name'        => 'Admin001',
-                'email'       => 'erwinarellano53@gmail.com',
-                'user_role_id'=> 1, // Admin role
-                'created_at'  => Carbon::now(),
-                'updated_at'  => Carbon::now(),
+                'name'              => 'Admin001',
+                'user_id'           => 'admin001',
+                'email'             => 'erwinarellano53@gmail.com',
+                'role_id'           => 1, // Admin role
+                'remember_token'    => null,
+                'email_verified_at' => now(),
+                'created_at'        => Carbon::now(),
+                'updated_at'        => Carbon::now(),
             ],
             [
-                'name'        => 'Testing Laravel',
-                'email'       => 'testinglaravel001@gmail.com',
-                'user_role_id'=> 1, // Admin role
-                'created_at'  => Carbon::now(),
-                'updated_at'  => Carbon::now(),
+                'name'              => 'Testing Laravel',
+                'user_id'           => 'testinglaravel001',
+                'email'             => 'testinglaravel001@gmail.com',
+                'role_id'           => 1, // Admin role
+                'remember_token'    => null,
+                'email_verified_at' => now(),
+                'created_at'        => Carbon::now(),
+                'updated_at'        => Carbon::now(),
             ],
         ];
 
         foreach ($admins as $admin) {
-            $existing = DB::table('admins')->where('email', $admin['email'])->first();
+            $existing = DB::table('users')->where('user_id', $admin['user_id'])->first();
             if ($existing) {
-                $this->command->info("Admin already exists: {$admin['email']}. Skipping.");
+                $this->command->info("Admin already exists: {$admin['user_id']}. Skipping.");
                 continue;
             }
-            DB::table('admins')->insert($admin);
-            $this->command->info("✅ Admin created: {$admin['email']}");
+            DB::table('users')->insert($admin);
+            $this->command->info("✅ Admin created: {$admin['user_id']}");
         }
     }
 }
