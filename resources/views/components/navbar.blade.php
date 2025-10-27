dac74e9e029bb9eafdf3be590e720187a7744944
Jerwin Uman <erwinarellano53@gmail.com>
2025-10-27 08:30:15 +0800

    update the side bar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,6 +5,7 @@
         <div class="sb-sidenav-menu">
             <div class="nav flex-column">
 
+                {{-- Default Sidebar Links --}}
                 <a class="nav-link" href="{{ route('admin.documents.dashboard') }}">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Dashboard
@@ -30,14 +31,16 @@
                     Logout
                 </a>
 
-                {{-- Additional links passed via slot --}}
+                {{-- Extra links passed via slot --}}
                 {{ $slot }}
+
             </div>
         </div>
 
+        {{-- Sidebar Footer --}}
         <div class="sb-sidenav-footer">
             <div class="small">Logged in as:</div>
-            <b title='Your Role'>{{ $role }}</b>
+            <b title="Your Role">{{ $role }}</b>
         </div>
     </nav>
 </div>
--- resources/views/components/private-layout.blade.php
+++ resources/views/components/private-layout.blade.php
@@ -1,6 +1,5 @@
 <!doctype html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,13 +7,13 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-        crossorigin="anonymous">
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossorigin="anonymous">
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
 
-    <!-- Custom Navbar CSS -->
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('build/css/navbar.css') }}">
 
     <!-- SweetAlert2 -->
@@ -22,39 +21,38 @@
 
     <!-- jQuery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+            integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
-
 <body class="sb-nav-fixed">
 
-    <div id="layoutSidenav">
-        {{-- Sidebar --}}
-        <x-navbar role="{{ auth()->user()->role }}">
-            {{-- Extra links passed via slot if needed --}}
-        </x-navbar>
+<!-- Top Navbar -->
+<x-nav-top />
 
-        {{-- Main Content --}}
-        <div id="layoutSidenav_content">
-            <x-nav-top></x-nav-top>
+<div id="layoutSidenav">
 
-            <div class="container-fluid mt-2">
-                {{ $slot }}
-            </div>
+    <!-- Sidebar -->
+    <x-navbar role="{{ auth()->user()->role }}">
+        {{-- Additional sidebar links can go here if needed --}}
+        {{ $sidebar ?? '' }}
+    </x-navbar>
+
+    <!-- Main Content -->
+    <div id="layoutSidenav_content">
+        <div class="container-fluid mt-2">
+            {{ $slot }}
         </div>
     </div>
 
-    {{-- Scripts --}}
-    <script src="{{ asset('build/js/navbar.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('build/js/navbar.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
         crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
-        crossorigin="anonymous"></script>
 </body>
-
 </html>
