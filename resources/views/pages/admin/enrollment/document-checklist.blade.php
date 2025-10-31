e2d6acaacd99799d6f816f77e7779b2db0095b27
Jerwin Uman <erwinarellano53@gmail.com>
2025-10-31 19:43:37 +0800

    kinder-kindergarten

--- resources/views/pages/admin/enrollment/document-checklist.blade.php
+++ resources/views/pages/admin/enrollment/document-checklist.blade.php
@@ -23,7 +23,7 @@
         <label for="gradeFilter" class="form-label fw-bold">Filter by Grade Level:</label>
         <select id="gradeFilter" class="form-select w-auto">
             <option value="">All Grades</option>
-            @foreach(['Kinder','Grade 1','Grade 2','Grade 3','Grade 4','Grade 5','Grade 6','Grade 7','Grade 8','Grade 9','Grade 10','Grade 11','Grade 12'] as $grade)
+            @foreach(['Kindergarten','Grade 1','Grade 2','Grade 3','Grade 4','Grade 5','Grade 6','Grade 7','Grade 8','Grade 9','Grade 10','Grade 11','Grade 12'] as $grade)
                 <option value="{{ $grade }}">{{ $grade }}</option>
             @endforeach
         </select>
